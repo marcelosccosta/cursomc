4b5f8ccf3ac04d8f2f1a5569bb62f43ef5c61b25
Marcelo Costa <marcelosantos20-13@hotmail.com>
2020-09-01 22:05:39 -0300

    ITEMPEDIDO E ITEMPEDIDOPK INSERIDOS E TESTADOS

--- src/main/java/com/marcelocosta/cursomc/domain/Produto.java
+++ src/main/java/com/marcelocosta/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.marcelocosta.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,28 +13,30 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-public class Produto implements Serializable{
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
-	
+
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA",
-	joinColumns        = @JoinColumn(name = "produto_id"),
-	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
-	
+
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	private Produto() {
-		
+
 	}
 
 	public Produto(Integer id, String nome, Double preco) {
@@ -74,6 +78,14 @@ public class Produto implements Serializable{
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -98,7 +110,5 @@ public class Produto implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 
 }
