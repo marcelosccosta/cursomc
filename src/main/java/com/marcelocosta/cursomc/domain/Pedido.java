7b592cfb46b1549fcf8a85a6ad5d7b168813dd77
Marcelo Costa <marcelosantos20-13@hotmail.com>
2020-09-01 22:53:12 -0300

    UPDATE ANOTACAO JSON IGNORE

--- src/main/java/com/marcelocosta/cursomc/domain/Pedido.java
+++ src/main/java/com/marcelocosta/cursomc/domain/Pedido.java
@@ -1,10 +1,8 @@
 package com.marcelocosta.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -18,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido  implements Serializable{
@@ -31,11 +28,9 @@ public class Pedido  implements Serializable{
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
 	
-	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
