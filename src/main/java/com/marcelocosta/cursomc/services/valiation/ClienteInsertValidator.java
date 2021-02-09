54e6033464838dad32bb84cdae6adaec20bce43c
Marcelo Costa <marcelosantos20-13@hotmail.com>
2021-02-08 22:12:51 -0300

    VALIDAÇAO CUSTOMIZADA EMAIL EXISTENTE

--- src/main/java/com/marcelocosta/cursomc/services/valiation/ClienteInsertValidator.java
+++ src/main/java/com/marcelocosta/cursomc/services/valiation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marcelocosta.cursomc.domain.Cliente;
 import com.marcelocosta.cursomc.domain.enums.TipoCliente;
 import com.marcelocosta.cursomc.dto.ClienteNewDTO;
+import com.marcelocosta.cursomc.repositories.ClienteRepository;
 import com.marcelocosta.cursomc.resources.exceptions.FieldMessage;
 import com.marcelocosta.cursomc.services.valiation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository clienterepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}	
@@ -21,10 +29,15 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		List<FieldMessage> list = new ArrayList<>();
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF invélido"));
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
 		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invélido"));
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienterepository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado no sistema!"));
 		}
 		
 		for (FieldMessage e : list) {
