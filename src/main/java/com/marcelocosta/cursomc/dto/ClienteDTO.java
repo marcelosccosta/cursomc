16a13c9f362b478c3a357980479b78d4048c2d2c
Marcelo Costa <marcelosantos20-13@hotmail.com>
2021-02-08 22:55:49 -0300

    VALIDAÇAO CUSTOMIZADA EMAIL EXISTENTE NA ATUALIZAÇÃO DE CLIENTE

--- src/main/java/com/marcelocosta/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/marcelocosta/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.marcelocosta.cursomc.domain.Cliente;
+import com.marcelocosta.cursomc.services.valiation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;	
 
